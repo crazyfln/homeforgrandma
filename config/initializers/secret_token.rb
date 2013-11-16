b7c808fa69d0fd01e63e08f760317233e5e2aca1
crazyfln <jeff.wge@gmail.com>
2013-11-16 11:26:53 -0800

    bulala

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HomeForGrandma::Application.config.secret_key_base = '8877d3cc271cb41dd9bc81752dcddaf428b72a3011e9afe22da7630df981ace2135a5b8398e325b5fcd71f9bb3e4aadeb9c0fae82f335b018bf767be432f5b3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HomeForGrandma::Application.config.session_store :cookie_store, key: '_HomeForGrandma_session'
